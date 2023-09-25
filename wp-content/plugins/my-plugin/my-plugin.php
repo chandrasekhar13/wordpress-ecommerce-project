6aed18a9e90c49796df630ecdb5a643d21d18556
chandrasekhar <chandusushil13@gmail.com>
2023-09-25 10:42:05 +0530

    Initial Commit

---
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: My Plugin
+ * Description: Test Description
+ * Version: 1.0
+ * Author: chandra sekhar
+ * Author URI: https://www.google.com
+ */
+if (!defined('ABSPATH')) {
+    header('Location: /wordpress');
+}
